8c88e27db11dbc115ae178dc822ff308579ad23b
Sebastian Diaz <alu0100893649@ull.edu.es>
2017-10-23 23:56:34 +0000

    Integración - lipidos

--- lib/Alimento.rb
+++ lib/Alimento.rb
@@ -1,11 +1,12 @@
 require "Alimento/version"
 
 class Alimento
-    attr_accessor :nombre, :glucidos, :proteinas
+    attr_accessor :nombre, :glucidos, :proteinas, :lipidos
     
     def initialize(nombre, glucidos, proteinas, lipidos)
         @nombre = nombre
         @glucidos = glucidos
         @proteinas = proteinas
+        @lipidos = lipidos
     end
 end
