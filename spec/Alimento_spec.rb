c372da5d454c2c49856c89ea415526716e222bc5
Sebastian Diaz <alu0100893649@ull.edu.es>
2017-10-26 10:07:50 +0000

    Modificación TDD - inclusión de before :all

--- spec/Alimento_spec.rb
+++ spec/Alimento_spec.rb
@@ -1,35 +1,40 @@
 require "spec_helper"
 
 RSpec.describe Alimento do
+  before :all do
+    @manzana = Alimento.new("manzana", 10, 10, 10)
+    @pera = Alimento.new("pera", 1, 2, 3)
+  end
+  
   it "has a version number" do
     expect(Alimento::VERSION).not_to be nil
   end
   context "Elementos básicos del alimento" do
     it "Tiene los atributos nombre, proteinas, glucidos y lipidos" do
-      expect(Alimento.new("manzana", 10, 10, 10)).to have_attributes(:nombre => "manzana", :proteinas => 10, :glucidos => 10, :lipidos => 10)
+      expect(@manzana).to have_attributes(:nombre => "manzana", :proteinas => 10, :glucidos => 10, :lipidos => 10)
     end
     it "Tiene un método para obtener el nombre" do
-      expect(Alimento.new("manzana", 10, 10, 10).nombre).to eq("manzana")
+      expect(@manzana.nombre).to eq("manzana")
     end
     it "Tiene un método para obtener el glucidos" do
-      expect(Alimento.new("manzana", 10, 10, 10).glucidos).to eq(10)
+      expect(@manzana.glucidos).to eq(10)
     end
     it "Tiene un método para obtener el proteinas" do
-      expect(Alimento.new("manzana", 10, 10, 10).proteinas).to eq(10)
+      expect(@manzana.proteinas).to eq(10)
     end
     it "Tiene un método para obtener el lipidos" do
-      expect(Alimento.new("manzana", 10, 10, 10).lipidos).to eq(10)
+      expect(@manzana.lipidos).to eq(10)
     end
   end
   
   context "Formateo del alimento" do
     it "Tiene un método para obtener el alimento formateado" do
-      expect(Alimento.new("manzana", 10, 10, 10).to_s).to eq("manzana: 10g proteínas, 10g glúcidos, 10g lípidos")
+      expect(@manzana.to_s).to eq("manzana: 10g proteínas, 10g glúcidos, 10g lípidos")
     end
   end
   context "Valor energético" do
     it "Tiene un método para obtener el valor energético del alimento" do
-      expect(Alimento.new("manzana", 1, 2, 3).kcal).to eq(2*4 + 1*4 + 3*9)
+      expect(@pera.kcal).to eq(2*4 + 1*4 + 3*9)
     end
   end
   
