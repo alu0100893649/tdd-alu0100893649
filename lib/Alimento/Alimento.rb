3bcec8a803e91c6a4c2d9dd9295bd5a5014912aa
Sebastian Diaz <alu0100893649@ull.edu.es>
2017-11-04 22:43:32 +0000

    Arreglando estructura de trabajo

---
+++ lib/Alimento/Alimento.rb
@@ -0,0 +1,19 @@
+class Alimento
+    attr_reader :nombre, :glucidos, :proteinas, :lipidos
+    
+    def initialize(nombre, glucidos, proteinas, lipidos)
+        @nombre = nombre
+        @glucidos = glucidos
+        @proteinas = proteinas
+        @lipidos = lipidos
+    end
+    
+    def to_s
+        x = "#{nombre}: #{proteinas}g proteínas, #{glucidos}g glúcidos, #{lipidos}g lípidos"
+        return x
+    end
+    
+    def kcal
+        @proteinas * 4 + @glucidos * 4 + @lipidos * 9
+    end
+end
