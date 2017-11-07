c54b64f5d5b7bd4312cd17ae0aacbe59c02ab0fb
Sebastian Diaz <alu0100893649@ull.edu.es>
2017-11-07 16:36:02 +0000

    TDD - Extraer nodo de la cabeza

--- spec/Lista_spec.rb
+++ spec/Lista_spec.rb
@@ -35,6 +35,11 @@ RSpec.describe Lista do
         expect(@list1.head.next.value).to eq(2)
         expect(@list1.head.next.next.value).to eq(3)
     end
+    
+    it "puede extraer el nodo de la cabeza" do
+        @list1.push_head(5)
+        expect(@list1.pop_head).to eq(5)
+    end
   end
   
 end
