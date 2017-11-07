f074947867d2b22e48833108e49ab8d4be31f7aa
Sebastian Diaz <alu0100893649@ull.edu.es>
2017-11-07 15:58:19 +0000

    TDD - nodo

--- spec/Lista_spec.rb
+++ spec/Lista_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Lista do
   it "has a version number" do
     expect(Lista::VERSION).not_to be nil
   end
+  
+  context "Nodo" do
+    it "tiene atributos value, next y prev" do
+        expect(Node.new(15, nil, nil)).to have_attributes(:value => 15, :next => nil, :prev => nil)
+    end
+  end
+  
 end
