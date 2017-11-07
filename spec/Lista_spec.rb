99232b842a34b827f2f7a5e5f0a6b2695092bee9
Sebastian Diaz <alu0100893649@ull.edu.es>
2017-11-07 16:03:20 +0000

    TDD - Atributos cabeza y cola de lista

--- spec/Lista_spec.rb
+++ spec/Lista_spec.rb
@@ -1,13 +1,23 @@
 require "spec_helper"
 
 RSpec.describe Lista do
+  before :all do
+    @nodo1 = Node.new(15, nil, nil)
+    @list1 = Lista.new()
+  end
   it "has a version number" do
     expect(Lista::VERSION).not_to be nil
   end
   
   context "Nodo" do
     it "tiene atributos value, next y prev" do
-        expect(Node.new(15, nil, nil)).to have_attributes(:value => 15, :next => nil, :prev => nil)
+        expect(@nodo1).to have_attributes(:value => 15, :next => nil, :prev => nil)
+    end
+  end
+  
+  context "Lista" do
+    it "tiene cabeza y cola" do
+        expect(@list1).to have_attributes(:head => nil, :tail => nil)
     end
   end
   
