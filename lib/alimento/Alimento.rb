dbe0cf7a8ceb9c7b9b57b31f21ae4598c2e3e4cc
Sebastian Diaz <alu0100893649@ull.edu.es>
2017-11-24 03:21:31 +0000

    Implementación indice glucemico funcional

--- lib/alimento/Alimento.rb
+++ lib/alimento/Alimento.rb
@@ -25,7 +25,35 @@ class Alimento
     end
     
     def calculate_index(alimento, glucosa)
+		aibc = []
+		aibcgl = []
+		acum = 0
 		
+		alimento.each do |x|
+			x.each_index do |a| 
+				if (a  != 0) 
+					acum += (((x[a] - x[0]) + (x[a - 1] - x[0])) /2)*5  
+				end 
+			end 
+			aibc << acum
+			acum = 0
+		end
+		
+		glucosa.each do |x|
+			acum = 0
+			x.each_index do |a| 
+				if (a  != 0) 
+					acum += (((x[a] - x[0]) + (x[a - 1] - x[0])) /2)*5
+				end 
+			end 
+			aibcgl << acum
+			acum = 0
+		end
+
+		igind = []
+		igind = aibc.map.with_index { |a, x| a = ((aibc[x] / aibcgl[x]) * 100) }
+	    
+	    igind.reduce(:+) / igind.length
     end
     
     # Convierte el objeto en un String
