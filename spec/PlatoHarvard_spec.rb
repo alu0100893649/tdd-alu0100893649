411892c18191b35fba74c4944ac2f6ffdaaacbaa
Sebastian Diaz <alu0100893649@ull.edu.es>
2017-12-13 23:50:55 +0000

    TDD - Construcción del PlatoHarvard - name

--- spec/PlatoHarvard_spec.rb
+++ spec/PlatoHarvard_spec.rb
@@ -2,9 +2,16 @@ require "spec_helper"
 
 RSpec.describe PlatoHarvard do
   before :all do
+      @plato = PlatoHarvard.new("Plato de Ejemplo")
   end
   
   it "has a version number" do
     expect(PlatoHarvard::VERSION).not_to be nil
+  end
+  
+  context "#attributes" do
+    it "#name" do
+        expect(@plato).to have_attributes(:name => "Plato de Ejemplo");
+    end
   end
 end
